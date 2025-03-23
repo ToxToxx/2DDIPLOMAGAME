e659d2b6b04f591b863012deb5a1ba2d9da5c04e
Matvey <sinema3d00@gmail.com>
2025-03-23 14:26:25 +0300

    making player interaction

--- Assets/Game/Scripts/InputManager/InputManager.cs
+++ Assets/Game/Scripts/InputManager/InputManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +5,7 @@ public class InputManager : MonoBehaviour
 {
     public static PlayerInput PlayerInput;
 
+    [Header("Movement")]
     public static Vector2 Movement;
     public static bool JumpWasPressed;
     public static bool JumpIsHeld;
@@ -14,10 +13,16 @@ public class InputManager : MonoBehaviour
     public static bool RunIsHeld;
     public static bool DashWasPressed;
 
+
     private InputAction _moveAction;
     private InputAction _jumpAction;
     private InputAction _runAction;
     private InputAction _dashAction;
+
+    [Header("Interaction")]
+    public static bool InteractionWasPressed;
+
+    private InputAction _interactionAction;
 
     private void Awake()
     {
@@ -26,20 +31,23 @@ public class InputManager : MonoBehaviour
         _moveAction = PlayerInput.actions["Move"];
         _jumpAction = PlayerInput.actions["Jump"];
         _runAction = PlayerInput.actions["Run"];
-
         _dashAction = PlayerInput.actions["Dash"];
+
+        _interactionAction = PlayerInput.actions["Interact"];
+
+
     }
 
     private void Update()
     {
         Movement = _moveAction.ReadValue<Vector2>();
-
         JumpWasPressed = _jumpAction.WasPressedThisFrame();
         JumpIsHeld = _jumpAction.IsPressed();
         JumpWasReleased = _jumpAction.WasReleasedThisFrame();
-
         RunIsHeld = _runAction.IsPressed();
-
         DashWasPressed = _dashAction.WasPressedThisFrame();
+
+        InteractionWasPressed = _interactionAction.WasPressedThisFrame();
+
     }
 }
