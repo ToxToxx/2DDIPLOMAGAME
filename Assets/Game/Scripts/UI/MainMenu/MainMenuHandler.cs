00083ce2a4244e51aa192e6057ceb71ea12ea6fd
Matvey <sinema3d00@gmail.com>
2025-05-06 21:40:29 +0300

    add sound effects

---
+++ Assets/Game/Scripts/UI/MainMenu/MainMenuHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace MainMenu
+{
+    public class MainMenuHandler : MonoBehaviour
+    {
+        private void Start()
+        {
+            Time.timeScale = 1.0f;
+        }
+
+    }
+
+}
